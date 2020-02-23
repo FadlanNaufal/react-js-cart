f89fd747be1e27b301f0387ba9c5faec67bd11fb
FadlanNaufal <fadlannaufal6969@gmail.com>
2020-02-23 10:06:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const Cate = "Sneakers"
+  const Title = "Green Saber Compass"
+  const Price = 7500000
+  const Info = "One of the most wanted shoes of the world!"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Parentbox">
+      <div className="Foto">
+        <img src="https://cdn.idntimes.com/content-images/post/20200128/76879332-180124689710577-4143807610172321531-n-2f2848d3522dccb9a7759d5a1c385631_600x400.jpg" />
+      </div>
+      <div className="Deskripsi">
+        <p class="Cate">{Cate}</p>
+        <h1 class="Title">{Title}</h1>
+        <p class="Price">{Price}</p>
+        <p class="Info">
+          {Info}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
